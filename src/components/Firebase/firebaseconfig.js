d2400686ae1f2ffa07b83f8526c40e7ddef4b973
shakil89427 <shakilahmed89427@gmail.com>
2021-12-02 13:17:05 +0600

    env added

--- src/components/Firebase/firebaseconfig.js
+++ src/components/Firebase/firebaseconfig.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyCPMgcm5S87aekGj9fNiusYUVXy-AZaivk",
-    authDomain: "gpushop-8652e.firebaseapp.com",
-    projectId: "gpushop-8652e",
-    storageBucket: "gpushop-8652e.appspot.com",
-    messagingSenderId: "483685483578",
-    appId: "1:483685483578:web:e87eb9197b1c762b53e103"
-  };
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_FIREBASE_APPID,
+};
 
-  export default firebaseConfig;
+export default firebaseConfig;
